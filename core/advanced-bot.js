7d78238f4301f488894dc36832382d36f6cee130
itxreyanon <itxreyanon@gmail.com>
2025-07-25 12:59:15 +0500

    Update advanced-bot.js

--- core/advanced-bot.js
+++ core/advanced-bot.js
@@ -1,6 +1,6 @@
 import { IgApiClient } from 'instagram-private-api';
 import { withRealtime, withFbns, withFbnsAndRealtime } from 'instagram_mqtt';
-import { GraphQLSubscriptions, SkywalkerSubscriptions } from 'instagram_mqtt/dist/realtime/subscriptions.index';
+import { GraphQLSubscriptions, SkywalkerSubscriptions } from 'instagram_mqtt/dist/realtime/subscriptions/index.js';
 
 import { Logger } from './logger.js';
 import { SessionManager } from './session-manager.js';
